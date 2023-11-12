7e4ec8a6879d01f928be700924aad00a4918b688
soham-bedi <bediss@rknec.edu>
2023-11-12 12:38:15 +0530

    change

--- connect.php
+++ connect.php
@@ -3,19 +3,17 @@
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $adult = $_POST['adult'];
-    $child = $_POST['child'];
+    $child = $_POST['children']; // corrected the name here
     $indate = $_POST['indate'];
     $outdate = $_POST['outdate'];
-    $roompref = $_POST['roompref'];
+    $roompref = $_POST['room_preference']; // corrected the name here
 
-    $conn=new mysqli('localhost', 'root', '', 'dbms_project');
-    if($conn->connect_error)
-    {
-        die('connection failed:' .$conn->connect_error);
-    }
-    else{
+    $conn = new mysqli('localhost', 'root', '', 'dbms_project');
+    if ($conn->connect_error) {
+        die('Connection failed: ' . $conn->connect_error);
+    } else {
         $stmt = $conn->prepare("INSERT INTO guests (name, email, phone, adult, child) VALUES (?, ?, ?, ?, ?)");
-        
+
         // Bind parameters
         $stmt->bind_param("ssiii", $name, $email, $phone, $adult, $child);
 
